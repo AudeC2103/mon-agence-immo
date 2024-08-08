4da3ce2c6b082ccb12543f044d14eb43b249b843
AudeC2103 <aude.campillo@gmail.com>
2024-08-08 16:26:04 +0200

    Commit1

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,34 +1,45 @@
-// src/components/Carrousel/Carrousel.jsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
-import './style.module.scss'
+import { PropertiesAPI } from '../../api/properties-api';
+import styles from './style.module.scss';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const Carrousel = ({ properties }) => {
+
+const Carrousel = () => {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    PropertiesAPI.fetchAllProperties().then(data => {
+      setProperties(data);
+    }).catch(error => {
+      console.error("Erreur lors de la récupération des propriétés:", error);
+    });
+  }, []);
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 2, // 2 propriétés visibles en mode desktop
     slidesToScroll: 1,
     responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 1 } },
-      { breakpoint: 768, settings: { slidesToShow: 2, slidesToScroll: 1 } },
+      { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 1 } },
+      { breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1 } },
       { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
     ]
   };
 
   return (
-    <div className="carrousel-container">
+    <div className={styles.carrouselContainer}>
       <Slider {...settings}>
         {properties.map(property => (
-          <div key={property.id} className="carrousel-slide">
-            <img src={property.images[0]} alt={property.title} className="carrousel-image" />
-            <div className="carrousel-caption">
+          <div key={property.id} className={styles.carrouselSlide}>
+            <img src={property.images[0]} alt={property.title} className={styles.carrouselImage} />
+            <div className={styles.carrouselCaption}>
               <h3>{property.title}</h3>
               <p>{property.description}</p>
-              <div className="property-details">
+              <div className={styles.propertyDetails}>
                 <p>{property.city}</p>
                 <p>{property.price} {property.currency}</p>
                 <p>{property.area} m²</p>
