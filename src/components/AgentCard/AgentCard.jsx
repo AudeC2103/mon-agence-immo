7562560b6a2df086371f5658b13d24cbf42f0f27
AudeC2103 <aude.campillo@gmail.com>
2024-08-29 14:36:22 +0200

    20240829 - Commit2

--- src/components/AgentCard/AgentCard.jsx
+++ src/components/AgentCard/AgentCard.jsx
@@ -2,6 +2,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import s from './style.module.scss';
+import Email from './../../assets/icones/email.png'
+import Phone from './../../assets/icones/phone.png'
+import Info from './../../assets/icones/info.png'
 
 const AgentCard = ({ agent }) => {
   return (
@@ -10,12 +13,24 @@ const AgentCard = ({ agent }) => {
         <img src={agent.photo} alt={`${agent.firstName} ${agent.lastName}`} className={s.image} />
         <div className={s.details}>
           <h4 className={s.name}>{agent.firstName} {agent.lastName}</h4>
-          <h5 className={s.phone}>{agent.phone}</h5>
+          <div className={s.phone}>
+            <img src={Phone} className={s.iconPhone} alt="icone telephone" />
+            <p className={s.phoneNumber}>{agent.phone} </p>
+          </div>
         </div>
       </div>
       <div className={s.description}>
-        <h6 className={s.email}>{agent.email}</h6>
-        <p>{agent.description}</p>
+        <div className={s.email}>
+          <img src={Email} className={s.iconMail} alt="icone e-mail" />
+          <p className={s.emailAdress}>{agent.email}</p>
+        </div>
+        <div className={s.infoText}>
+          <img src={Info} className={s.iconInfo} alt="icone info" />
+          <p>{agent.description} </p>
+        </div>
+        
+        
+        
       </div>
     </div>
   );
